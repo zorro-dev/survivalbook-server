3b6e76e925fcc2db87cd4f59b5ac5b85f4f42274
Artem <zorkoart123@gmail.com>
2021-11-26 08:05:52 +0100

    add release server to docs

--- docs/servers.js
+++ docs/servers.js
@@ -8,7 +8,9 @@ module.exports = {
             url: "https://survival-book.herokuapp.com/api", // url
             description: "Release test server", // name
         },
-
-
+        {
+            url: "http://62.113.96.206:5000/",
+            description: "Release server",
+        }
     ],
 }
